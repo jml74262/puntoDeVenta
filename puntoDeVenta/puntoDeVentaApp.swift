ca9da61290ce12763e34d9aa21b651aa75c41fbe
jml74262 <jml74262@lasallebajio.edu.mx>
2023-04-24 18:32:14 -0600

    Initial Commit

---
+++ puntoDeVenta/puntoDeVentaApp.swift
@@ -0,0 +1,17 @@
+//
+//  puntoDeVentaApp.swift
+//  puntoDeVenta
+//
+//  Created by ISSC_611_2023 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct puntoDeVentaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ puntoDeVenta/purchaseView.swift
@@ -0,0 +1,58 @@
+//
+//  purchaseView.swift
+//  puntoDeVenta
+//
+//  Created by ISSC_611_2023 on 24/04/23.
+//
+
+import SwiftUI
+import Combine
+
+struct purchaseView: View {
+    @State private var productID = ""
+    @State private var name = ""
+    @State private var pieces = ""
+
+    @State private var mostrarAlerta = false
+    
+    var body: some View {
+        VStack{
+            Text("Purchase").font(.largeTitle).padding()
+            
+            Form{
+                TextField("Product ID", text: $productID)
+                           .keyboardType(.numberPad)
+                           .onReceive(Just(productID)) { value in
+                               let filtered = "\(value)".filter { "0123456789".contains($0) }
+                               if filtered != value {
+                                   self.productID = "\(filtered)"
+                               }
+                           }
+                
+                TextField("Name",text: $name)
+                    .onChange(of: name) { value in
+                        if !value.allSatisfy({ $0.isLetter }) {
+                            name = String(value.filter { $0.isLetter })
+                        }
+                    }
+                TextField("Pieces", text: $pieces)
+                
+            }.padding()
+            Button("Register"){
+                if !productID.isEmpty  && !name.isEmpty && !pieces.isEmpty {
+                    mostrarAlerta = true
+                }
+                
+            }.padding().alert(isPresented:$mostrarAlerta){
+                Alert(title: Text("Hola"),
+                message: Text("Has presionado el boton"))
+            }
+        }
+    }
+}
+
+struct purchaseView_Previews: PreviewProvider {
+    static var previews: some View {
+        purchaseView()
+    }
+}
